17e4275aec0b9bab74705897b8b1521a49871a24
SimonaMartinucci <simo.martinucci3@gmail.com>
2024-07-05 14:04:14 +0200

    feat: deleted useless files + add imgs + create first component

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,48 @@
+<script>
+    export default {
+        name: "AppHeader",
+    }
+</script>
+
+
+<template>
+    <header>
+        <div class="cont-header">
+            <div class="header-logo"></div>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+</template>
+
+
+<style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
+@use '../styles/partials/mixins' as *;
+
+header {
+    width: 100%;
+    background-color: white;
+    height: 100px;
+
+    .cont-header {
+        width: 80%;
+        margin: 0 auto;
+        // DEBUG
+        height: 100%;
+        background-color: blue;
+    }
+}
+
+</style>
