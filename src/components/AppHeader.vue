e750d024c6cbbc336ece5e5475a5afba80dee6d6
SimonaMartinucci <simo.martinucci3@gmail.com>
2024-07-05 14:38:20 +0200

    feat: add header component (template + style)

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -8,17 +8,19 @@
 <template>
     <header>
         <div class="cont-header">
-            <div class="header-logo"></div>
+            <div class="header-logo">
+                <img src="../assets/dc-logo.png" alt="logo">
+            </div>
             <nav>
                 <ul>
                     <li>
-                        <a href="#">Home</a>
+                        <a href="#">HOME</a>
+                    </li>
+                    <li class="active">
+                        <a href="#">HOME</a>
                     </li>
                     <li>
-                        <a href="#">Home</a>
-                    </li>
-                    <li>
-                        <a href="#">Home</a>
+                        <a href="#">HOME</a>
                     </li>
                 </ul>
             </nav>
@@ -36,13 +38,57 @@ header {
     background-color: white;
     height: 100px;
 
+    a {
+        text-decoration: none;
+        color: $light_gray;
+    }    
+
     .cont-header {
-        width: 80%;
-        margin: 0 auto;
-        // DEBUG
+        width: 70%;
         height: 100%;
-        background-color: blue;
-    }
+        margin: 0 auto;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        .header-logo {
+        width: 80px;
+
+            img {
+                max-width: 100%;
+            }
+        }
+
+        nav {
+            height: 100%;
+
+            ul {
+            list-style-type: none; 
+            display: flex;
+            // gap: 15px;
+            height: 100%;
+
+                li {
+                    height: 100%;
+                    align-content: center;
+                    padding: 0 15px;
+                }
+
+                li:hover {
+                    background-color: rgb(239, 238, 238);
+                    cursor: pointer;
+                }
+
+                li.active {
+                    border-bottom: 3px solid $light_blue;
+                }
+
+                li.active a {
+                    color: $light_blue;
+                }
+            }
+        }
+    }       
 }
 
 </style>
